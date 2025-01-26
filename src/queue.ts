f6161fcf63bcf358ff1617d25f93a481ae047392
meta-human-08 <1032210418@tcetmumbai.in>
2025-01-27 01:15:22 +0530

    initialize project with BullMQ, Fastify, and BullBoard; add configuration files and basic queue setup

---
+++ src/queue.ts
@@ -0,0 +1,41 @@
+import { ConnectionOptions, Queue, QueueScheduler, Worker } from 'bullmq';
+
+import { env } from './env';
+
+const connection: ConnectionOptions = {
+  host: env.REDISHOST,
+  port: env.REDISPORT,
+  username: env.REDISUSER,
+  password: env.REDISPASSWORD,
+};
+
+export const createQueue = (name: string) => new Queue(name, { connection });
+
+export const setupQueueProcessor = async (queueName: string) => {
+  const queueScheduler = new QueueScheduler(queueName, {
+    connection,
+  });
+  await queueScheduler.waitUntilReady();
+
+  new Worker(
+    queueName,
+    async (job) => {
+      try {
+        const { name, email, accessToken, repoName } = job.data;
+        await fetch('https://git-name.onrender.com/generate-repo', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name, accessToken, email, repoName }),
+        });
+        return { jobId: `Job (${job.id}) completed successfully` };
+      } catch (error) {
+        console.log(error);
+      }
+
+      return { jobId: `This is the return value of job (${job.id})` };
+    },
+    { connection }
+  );
+};
